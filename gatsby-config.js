b96e186843ee896e08f1b049e1b03388b6c718ed
신유나 <yunadev01@gmail.com>
2021-04-22 01:22:48 +0900

    Add Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,42 @@
 module.exports = {
   siteMetadata: {
-    title: 'Find City',
+    title: "Find City",
+    description:
+      "This website is a test to find a destination that fits your preferences.",
   },
   plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-prettier-eslint',
+      resolve: "gatsby-plugin-prettier-eslint",
       options: {
         prettier: {
           patterns: [
             // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            '**/*.{css,scss,less}',
-            '**/*.{json,json5}',
-            '**/*.{graphql}',
-            '**/*.{md,mdx}',
-            '**/*.{html}',
-            '**/*.{yaml,yml}',
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
           ],
         },
         eslint: {
-          patterns: '**/*.{js,jsx,ts,tsx}',
+          patterns: "**/*.{js,jsx,ts,tsx}",
           customOptions: {
             fix: true,
             cache: true,
           },
         },
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
       },
     },
   ],
