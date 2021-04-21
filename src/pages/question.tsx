849f92fa059ae29ad1ab42b98187d95c960d7e74
신유나 <yunadev01@gmail.com>
2021-04-21 13:22:17 +0900

    Update pages

---
+++ src/pages/question.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const QuestionPage = () => {
+  <main>
+    <Helmet>
+      <title>찰떡궁합 도시 찾는중</title>
+    </Helmet>
+  </main>;
+};
+
+export default QuestionPage;
---
+++ src/pages/result.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const ResultPage = () => {
+  return (
+    <main>
+      <Helmet>
+        <title>찰떡궁합 도시 결과</title>
+      </Helmet>
+    </main>
+  );
+};
+
+export default ResultPage;
