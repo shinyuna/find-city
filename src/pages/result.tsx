c7712f5672397ee9da401c14712238d91efed432
신유나 <yunadev01@gmail.com>
2021-04-23 00:58:39 +0900

    Update result page

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React, { useEffect, useState } from "react";
+import { graphql, Link, PageProps, useStaticQuery } from "gatsby";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import Layout, { pointColor } from "../components/layout";
 
-const ResultPage = () => {
+interface IMap {
+  publicURL: string;
+}
+
+const Result = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  img {
+    width: 100%;
+  }
+`;
+const Button = styled((props) => <Link {...props} />)`
+  display: block;
+  width: 90%;
+  padding: 1.5rem 0;
+  margin: 2rem auto;
+  border: none;
+  border-radius: 5rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+  text-align: center;
+  text-decoration: none;
+  color: #fff;
+  background-color: ${pointColor};
+  cursor: pointer;
+`;
+
+const ResultPage: React.VFC<PageProps> = ({ location }) => {
+  const [img, setImg] = useState<string | null>(null);
+  const [, city] = location.search.split("=");
+  const {
+    images: { nodes: resultImages },
+  } = useStaticQuery(graphql`
+    {
+      images: allFile(filter: { base: { regex: "/result/" } }) {
+        nodes {
+          publicURL
+        }
+      }
+    }
+  `);
+  useEffect(() => {
+    const { publicURL: resultImg } = resultImages.find((item: IMap) =>
+      item.publicURL.includes(city)
+    );
+    setImg(resultImg);
+  }, [img, location]);
   return (
-    <main>
+    <Layout>
       <Helmet>
         <title>찰떡궁합 도시 결과</title>
       </Helmet>
-    </main>
+      <Result>
+        {img && <img src={img} alt={city} />}
+        <Button to="/qna">테스트 다시 해보기</Button>
+      </Result>
+    </Layout>
   );
 };
 
