a59842a07bd59b13bb137eb9685b188990f7ae52
신유나 <yunadev01@gmail.com>
2021-04-25 15:07:47 +0900

    Update jest setting

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path")
-
 module.exports = {
   transform: {
     "^.+\\.(tsx?|jsx?)$": `<rootDir>/tests/jest-preprocess.js`,
   },
   moduleNameMapper: {
-    // "\\.svg": `./tests/__mocks__/file-mocks.js`,
     "typeface-*": "identity-obj-proxy",
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/tests/__mocks__/file-mocks.js`,
@@ -19,6 +17,10 @@ module.exports = {
   setupFilesAfterEnv: [path.resolve(__dirname, "./tests/setup-test-env.js")],
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js"],
-  collectCoverage: false,
+  collectCoverageFrom: [
+    `<rootDir>/src/components/**/*.tsx`,
+    `<rootDir>/src/pages/**/*.tsx`,
+  ],
+  collectCoverage: true,
   coverageReporters: ["lcov", "text", "html"],
 }
